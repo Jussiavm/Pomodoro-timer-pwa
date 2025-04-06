e71fc8f9590cec43e2b99f4547d17284a020bbe8
Jussi Mikkola <jussia.mikkola@gmail.com>
2025-04-06 14:59:15 +0300

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js').then(registration => {
+      navigator.serviceWorker.register('/Pomodoro-timer-pwa/sw.js').then(registration => {
         console.log('Service Worker registered with scope:', registration.scope);
       }).catch(error => {
         console.error('Service Worker registration failed:', error);
