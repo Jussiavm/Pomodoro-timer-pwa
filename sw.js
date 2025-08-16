99d69e57d2fda815da35fc32a6313bfdbb179ec7
Jussiavm <jussia.mikkola@gmail.com>
2025-08-16 17:52:17 +0300

    modified:   sw.js

--- sw.js
+++ sw.js
@@ -30,6 +30,21 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          // Delete old caches that are not the current one
+          return cacheName !== CACHE_NAME;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
